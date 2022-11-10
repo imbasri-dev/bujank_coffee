8c2bed78ba041d98afd4aceed21bbd56d7889aa2
imbasri.dev <imbasri.dev@gmail.com>
2022-11-10 22:22:14 +0700

    update cors default

--- index.js
+++ index.js
@@ -23,11 +23,7 @@ postgresDb
    .then(() => {
       console.log("Server Connected...!");
       // pasang parser untuk body
-      server.use(
-         cors({
-            origin: "*",
-         })
-      ); //cors options
+      server.use(cors()); //cors options
       server.use(express.static("./public")); // => data yang dipakai buat router get
       server.use(express.json());
       server.use(express.urlencoded({ extended: false }));
