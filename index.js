1933a4ffb51230e3a8cb2f2a6602d0c07b9d78d8
imbasri.dev <imbasri.dev@gmail.com>
2022-11-10 20:39:09 +0700

    update testing all cors

--- index.js
+++ index.js
@@ -23,14 +23,15 @@ postgresDb
    .then(() => {
       console.log("Server Connected...!");
       // pasang parser untuk body
-      server.use(
-         cors({
-            origin: "*",
-            methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-            preflightContinue: false,
-            optionsSuccessStatus: 204,
-         })
-      ); //cors options
+      server.use(function (req, res, next) {
+         res.header("Access-Control-Allow-Origin", "*");
+         res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+         );
+         next();
+      });
+      // server.use(cors()); //cors options
       server.use(express.static("./public")); // => data yang dipakai buat router get
       server.use(express.json());
       server.use(express.urlencoded({ extended: false }));
