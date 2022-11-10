434caa4bfc6994ac260a14c1d695950149e79ef4
imbasri.dev <imbasri.dev@gmail.com>
2022-11-10 21:06:42 +0700

    update

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const morgan = require("morgan");
 
 // cors untuk menerima perizin preflights
 const cors = require("cors");
-// const corsOptions = {
-//    "Access-Control-Allow-Origin": "*",
-//    credentials: true,
-//    optionsSuccesssStatus: 200,
-// };
+const corsOptions = {
+   origin: "*",
+   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+   preflightContinue: false,
+   optionsSuccessStatus: 204,
+};
 // importdb
 const postgresDb = require("./src/config/postgre");
 // import mainRouter
@@ -23,7 +24,7 @@ postgresDb
    .then(() => {
       console.log("Server Connected...!");
       // pasang parser untuk body
-      server.use(cors()); //cors options
+      server.use(cors(corsOptions)); //cors options
       server.use(express.static("./public")); // => data yang dipakai buat router get
       server.use(express.json());
       server.use(express.urlencoded({ extended: false }));
