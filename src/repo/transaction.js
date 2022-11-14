ba6771b0a892008279d5166c1d1664f77c341ba3
imbasri.dev <imbasri.dev@gmail.com>
2022-11-15 01:58:50 +0700

    update get transaction id

--- src/repo/transaction.js
+++ src/repo/transaction.js
@@ -14,7 +14,7 @@ const get = () => {
 };
 const historyUser = (token, queryParams) => {
    return new Promise((resolve, reject) => {
-      let query = `select u.email,pr.name,pr.image,pr.price,tr.status,tr.total from transactions tr inner join users u on tr.user_id = u.id inner join products pr on tr.product_id = pr.id inner join profiles pf on u.id = pf.user_id  where u.id = $1`;
+      let query = `select tr.id,u.email,pr.name,pr.image,pr.price,tr.status,tr.total from transactions tr inner join users u on tr.user_id = u.id inner join products pr on tr.product_id = pr.id inner join profiles pf on u.id = pf.user_id  where u.id = $1`;
 
       let queryLimit = "";
       let link = `http://localhost:5000/api/transaction/history?`;
